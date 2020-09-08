da6738c48d3f184a1c9ff03bfcbbd2b75f9e9c96
Jose Roman Alvarado <za17011562@zapopan.tecmm.edu.mx>
2020-09-08 18:23:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Mi nombre es:, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Mi numero de control es:')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+    print_hi('Jose Roman Alvarado Hernandez')
+
+
+
